05326068c4e50fca699d4b27fb029d0aa710b159
JuicyAdde <53942395+JuicyAdde@users.noreply.github.com>
2020-07-15 07:23:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { Client, MessageEmbed } = require('discord.js');
 const react = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 // Partials is the way to get uncached stuff from events.
 
-let { TOKEN, PREFIX } = process.env; 
+let PREFIX = ".";
 
 react.on('ready', () => {
   console.log("I am ready! :)")
 });
 
-react.login(TOKEN);
+react.login("YOUR_TOKEN");
 
 //Message Event, activates everytime a new message come.
 react.on('message', (message) => {
@@ -21,8 +21,9 @@ react.on('message', (message) => {
   let cmd = args.shift().toLowerCase();
   
   if (cmd === "create-embed") {
+    if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send("You need the **MANAGE_GUILD** permission to do this!")
     // Get your #self-roles channel id here.
-    let chx = react.channels.cache.get('732735432927739924');
+    let chx = react.channels.cache.get('SELF_ROLE_CHANNEL');
     
     // Make the embed, and send it.
     const embed = new MessageEmbed()
@@ -43,17 +44,17 @@ react.on('messageReactionAdd', async (reaction, user) => {
   if (user.bot) return; //Check if current user reacting is bot.
   if (!reaction.message.guild) return;
   // Put your guild ID here.
-  if (reaction.message.guild.id !== '732733414209355776') return;
+  if (reaction.message.guild.id !== 'GUILD_ID') return;
   
   //Self-role channel ID here.
-  if (reaction.message.channel.id === '732735432927739924') {
+  if (reaction.message.channel.id === 'SELF_ROLE_CHANNEL') {
     if (reaction.emoji.name === "🐶") {
       //Add role 1 (dog)
-      await reaction.message.guild.members.cache.get(user.id).roles.add("732737134284505158")
+      await reaction.message.guild.members.cache.get(user.id).roles.add("ROLE_1_ID")
     }
       //Add role 2 (cat)
     if (reaction.emoji.name === "🐱") {
-      await reaction.message.guild.members.cache.get(user.id).roles.add("732737167780216872")
+      await reaction.message.guild.members.cache.get(user.id).roles.add("ROLE_1_ID")
     }
   } else {
     return;
@@ -67,17 +68,17 @@ react.on('messageReactionRemove', async (reaction, user) => {
   if (user.bot) return; //Check if current user reacting is bot.
   if (!reaction.message.guild) return;
   // Put your guild ID here.
-  if (reaction.message.guild.id !== '732733414209355776') return;
+  if (reaction.message.guild.id !== 'GUILD_ID') return;
   
-  if (reaction.message.channel.id === '732735432927739924') {
+  if (reaction.message.channel.id === 'SELF_ROLE_CHANNEL') {
     if (reaction.emoji.name === "🐶") {
       //Remove role 1 (dog)
-      await reaction.message.guild.members.cache.get(user.id).roles.remove("732737134284505158")
+      await reaction.message.guild.members.cache.get(user.id).roles.remove("ROLE_1_ID")
     }
     
     if (reaction.emoji.name === "🐱") {
       //Remove role 1 (dog)
-      await reaction.message.guild.members.cache.get(user.id).roles.remove("732737167780216872")
+      await reaction.message.guild.members.cache.get(user.id).roles.remove("ROLE_2_ID")
     }
   } else {
     return;
